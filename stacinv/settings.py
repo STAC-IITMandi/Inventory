f230c01b35cdbc9639f9deb8f8e1f1c594f3192a
varunvaruns9 <varunjustrocks@gmail.com>
2018-05-18 13:56:17 +0530

    Add files for deployment

--- stacinv/settings.py
+++ stacinv/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%d@k7irdptc)vgz=-sk5e==5&%op()eqwrxjp&#dl)g1ybn4d('
+# SECRET_KEY = '%d@k7irdptc)vgz=-sk5e==5&%op()eqwrxjp&#dl)g1ybn4d('
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '%d@k7irdptc)vgz=-sk5e==5&%op()eqwrxjp&#dl)g1ybn4d(')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -42,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,3 +130,10 @@ EMAIL_HOST_USER = 'varunsinghs2021'
 EMAIL_HOST_PASSWORD = 'varunvaruns9'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
